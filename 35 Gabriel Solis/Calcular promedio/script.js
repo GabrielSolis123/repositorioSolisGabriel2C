a986fd9c0dc77873dd6cc295666b8959d69c45a2
GabrielSolis123 <gabrielsolis564@gmail.com>
2024-10-06 20:53:20 -0600

    Add files via upload

---
+++ 35 Gabriel Solis/Calcular promedio/script.js
@@ -0,0 +1,13 @@
+function quince(){
+    let promedio1 = +document.querySelector('#num1').value;
+    let promedio2 = +document.querySelector('#num2').value;
+    let promedio3= +document.querySelector('#num3').value;
+
+    let operacion1 = (promedio1*0.15);
+    let operacion2 = (promedio2*0.10);
+    let operacion3 = (promedio3*0.10);
+
+    let promedioFinal = (operacion1+operacion2+operacion3)
+
+    document.querySelector('#salida').textContent = 'tu promedio final es de:  '+promedioFinal;
+}
